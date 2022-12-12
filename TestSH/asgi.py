8b10da6868fda689fee2ad117ec1b0a3e5d77ac9
Vadim <vadim.petah@gmail.com>
2022-12-13 00:39:42 +0200

    first commit

---
+++ TestSH/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TestSH project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestSH.settings')
+
+application = get_asgi_application()
